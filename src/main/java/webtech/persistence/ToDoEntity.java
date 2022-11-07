82dc900eeeb75daaf5b73dbca72dcd34b1cd4664
Dean Mayhew <dean.mayhew123@gmail.com>
2022-11-07 11:48:29 +0100

    added ToDo entity for database

---
+++ src/main/java/webtech/persistence/ToDoEntity.java
@@ -0,0 +1,70 @@
+package webtech.persistence;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "todos")
+public class ToDoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "task", nullable = false)
+    private String task;
+
+    @Column(name = "isDone", nullable = false)
+    private boolean isDone;
+
+    @Column(name = "created", nullable = false)
+    private LocalDate created;
+
+    @Column(name = "dueTo")
+    private LocalDate dueTo;
+
+    public ToDoEntity(String task, boolean isDone, LocalDate created, LocalDate dueTo) {
+        this.task = task;
+        this.isDone = isDone;
+        this.created = created;
+        this.dueTo = dueTo;
+    }
+
+    protected ToDoEntity(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public LocalDate getDueTo() {
+        return dueTo;
+    }
+
+    public void setDueTo(LocalDate dueTo) {
+        this.dueTo = dueTo;
+    }
+}
